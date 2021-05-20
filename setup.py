b9b0e0ccc78bc1d9212abd4aa387895b268ad25b
shenshan <shenshanpku@gmail.com>
2021-05-20 06:31:46 +0000

    Ephys and histology notebook

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 setup(
     name='ibl-pipeline-light',
     version='0.1.1',
-    description='Light version of ibl pipelineto access data in IBL database',
+    description='Light version of ibl pipeline to access data in IBL database',
     author='Vathes',
     author_email='support@vathes.com',
     packages=find_packages(exclude=[]),
-    install_requires=['datajoint==0.12.8', 'ibllib'],
+    install_requires=['datajoint==0.12.9'],
 )
